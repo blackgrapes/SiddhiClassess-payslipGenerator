20b8f5c6ec8006dac017e3e4a2e69de952b4fb3f
shrivanshu <shrivanshu4@gmail.com>
2025-02-27 18:29:31 +0530

    student list

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,25 +4,23 @@ import Login from "./component/Login";
 import SignupPage from "./component/Signup";
 import ForgotPassword from "./component/ForgotPassword";
 import Home from "./component/Home";
-import  StudentDashboard from "./component/StudentDashboard";
-import  StudentList from "./component/StudentList";
+import AddStudent from "./component/AddStudent";
+import StudentList from "./component/StudentList";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
 function App() {
   return (
-    <Router>
-   
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/StudentDashboard" element={<StudentDashboard/>} />
-            <Route path="/StudentList" element={<StudentList/>} />
-          </Routes>
-        
+    <Router> {/* ✅ BrowserRouter added here */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/AddStudent" element={<AddStudent />} />
+        <Route path="/StudentList" element={<StudentList />} />
+      </Routes>
     </Router>
   );
 }
