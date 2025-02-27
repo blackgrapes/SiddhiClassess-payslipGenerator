1e973561f9f72cfe46073e28979ab7222701e9d8
0sourabh <sourabhsompandey@gmail.com>
2025-02-27 16:05:10 +0530

    teacher route added

--- Server-Side/server.js
+++ Server-Side/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const adminRoutes = require("./routes/adminRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const teacherRoutes = require("./routes/teacherRoutes"); 
 
 connectDB(); // Connect to MongoDB
 
@@ -17,6 +18,7 @@ app.use(cors());
 // Routes
 app.use("/admin", adminRoutes);
 app.use("/student", studentRoutes);
+app.use("/teacher", teacherRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
